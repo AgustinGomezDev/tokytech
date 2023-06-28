9a9a1efc65e8c2b0b1b75a67dad9bc13e82fb533
Agustin Gomez <agustingomezdev@gmail.com>
2023-06-28 12:20:54 -0300

    Fix product controller and view

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const ProductRouter = require('./products.router')
 const CartRouter = require('./carts.router')
-const viewsRouter = require('./views.router')
+const ViewRouter = require('./views.router')
 const SessionsRouter = require('./sessions.router')
 
 const productRouter = new ProductRouter()
 const cartRouter = new CartRouter()
-// const viewsRouter = new ViewsRouter()
+const viewRouter = new ViewRouter()
 const sessionsRouter = new SessionsRouter()
 
 const mainRouter = express.Router()
@@ -14,7 +14,7 @@ const mainRouter = express.Router()
 mainRouter.use('/api/products', productRouter.getRouter())
 mainRouter.use('/api/carts', cartRouter.getRouter())
 mainRouter.use('/api/sessions', sessionsRouter.getRouter())
-mainRouter.use('/', viewsRouter)
+mainRouter.use('/', viewRouter.getRouter())
 mainRouter.use('*', (req, res, next) => {
     res.status(404).send({status: "error", error: 'Requested path not found',});
 })
