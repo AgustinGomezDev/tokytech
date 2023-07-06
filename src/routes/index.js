50fe86cae803c57dd4e47b35f61fa5296351fc3f
Agustin Gomez <agustingomezdev@gmail.com>
2023-07-06 16:30:12 -0300

    Install fakerjs & add /mockingproducts

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const ProductRouter = require('./products.router')
 const CartRouter = require('./carts.router')
 const ViewRouter = require('./views.router')
 const SessionsRouter = require('./sessions.router')
+const { mockingProducts } = require('../utils/mockingProducts')
 
 const productRouter = new ProductRouter()
 const cartRouter = new CartRouter()
@@ -15,6 +16,9 @@ mainRouter.use('/api/products', productRouter.getRouter())
 mainRouter.use('/api/carts', cartRouter.getRouter())
 mainRouter.use('/api/sessions', sessionsRouter.getRouter())
 mainRouter.use('/', viewRouter.getRouter())
+mainRouter.use('/mockingproducts', (req, res, next) => {
+    res.send(mockingProducts())
+})
 mainRouter.use('*', (req, res, next) => {
     res.status(404).send({status: "error", error: 'Requested path not found',});
 })
