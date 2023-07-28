d58afab0adf779fd18688683bb15e842e25feff1
Agustin Gomez <agustingomezdev@gmail.com>
2023-07-28 10:16:46 -0300

    Add Swagger for documentation

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,6 +20,8 @@ mainRouter.use('/', viewRouter.getRouter())
 mainRouter.use('/mockingproducts', (req, res, next) => {
     res.send(mockingProducts())
 })
+
+// ---- Winston logger
 mainRouter.use('/loggerTest', (req, res, next) => {
     req.logger.fatal('testing fatal log')
     req.logger.error('testing error log')
@@ -29,9 +31,28 @@ mainRouter.use('/loggerTest', (req, res, next) => {
     req.logger.debug('testing debug log')
     res.send('Logger')
 })
+
+// ---- Swagger
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUiExpress = require('swagger-ui-express')
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: 'Documentation of my eCommerce api',
+            description: 'This is the documentation of my api that I have developed for an ecommerce in the Coderhouse backend course.'
+        }
+    },
+    apis: [`./docs/**/*.yaml`]
+}
+const specs = swaggerJsDoc(swaggerOptions)
+mainRouter.use('/api/docs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs))
+
 mainRouter.use('*', (req, res, next) => {
     res.status(404).send({status: "error", error: 'Requested path not found',});
 })
+
 mainRouter.use(errorHandler)
 
 module.exports = mainRouter
